23d95fc945bbbe3f1569b5e1a3bde39819d744bf
PDochev <plamendochev0@gmail.com>
2025-03-15 09:59:05 +0000

    Initial Commit

---
+++ components/NavBar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default async function NavBar() {
+  return (
+    <nav className="sticky top-0 z-10 bg-amber-50">
+      <ul className="w-full flex flex-row h-16 justify-between items-center p-4">
+        <li>
+          <Link
+            href="/"
+            className="flex items-center font-medium justify-center gap-2 py-3 px-4 "
+          >
+            <Image
+              src="/flood.svg"
+              alt="FloodWatch Logo"
+              className="rounded-full"
+              width={32}
+              height={32}
+              priority
+            />
+            FloodWatch
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
